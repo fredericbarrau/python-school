eec89fd2407ac081b9457652491e6813c6173ca1
BARRAU Frédéric <fbarrau@jouve.com>
2019-05-12 11:21:45 +0200

    presentation

---
+++ presentation.py
@@ -0,0 +1,27 @@
+# Ceci est un commentaire
+
+"ceci est une chaîne de caractère"
+
+'ceci est aussi une châine de caractère'
+
+3
+3.4
+545435.7856876
+0x2342
+
+# Afficher un message
+
+print("Ceci est un message")
+print(3)
+print(3454564.6345)
+
+
+# Variables
+
+ma_variable = "ceci est une variable"
+MaVariable = "Ceci est une AUTRE variable"
+
+# afficher une variable
+
+print(ma_variable)
+print(MaVariable)
